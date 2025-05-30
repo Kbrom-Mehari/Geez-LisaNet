d0377cf2ecc7e5cf67a8aa10be03fd5d20572d7f
Kbrom Mehari <kibrommehari121@gmail.com>
2025-05-30 17:04:40 +0300

    This class is used to make structured error response in the GlobalExceptionHandler class

--- src/main/java/org/kbapps/tigrinya_blog/exception/ErrorResponse.java
+++ src/main/java/org/kbapps/tigrinya_blog/exception/ErrorResponse.java
@@ -1,4 +1,33 @@
 package org.kbapps.tigrinya_blog.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+//@AllArgsConstructor
+@Data
 public class ErrorResponse {
+    private int status;
+    private String message;
+    private String error;
+    private LocalDateTime timestamp;
+    private Map<String,String> errors;
+
+    public  ErrorResponse(int status, String message, String error, LocalDateTime timestamp,Map<String,String> errors){
+        this.status=status;
+        this.message=message;
+        this.error=error;
+        this.timestamp=timestamp;
+        this.errors=errors;
+    }
+    public  ErrorResponse(int status, String message, String error, LocalDateTime timestamp){
+        this.status=status;
+        this.message=message;
+        this.error=error;
+        this.timestamp=timestamp;
+    }
 }
+
+
