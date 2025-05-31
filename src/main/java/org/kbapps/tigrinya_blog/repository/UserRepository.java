76268b15a95a639a1202767a6c5deb5e6b08b33b
Kbrom Mehari <kibrommehari121@gmail.com>
2025-05-31 11:43:27 +0300

    added two methods to check email and username exist

--- src/main/java/org/kbapps/tigrinya_blog/repository/UserRepository.java
+++ src/main/java/org/kbapps/tigrinya_blog/repository/UserRepository.java
@@ -4,4 +4,6 @@ import org.kbapps.tigrinya_blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
