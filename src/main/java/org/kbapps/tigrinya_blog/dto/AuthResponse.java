dbd6039eb5b43b875dd9973b3df1eb010e555dc5
Kbrom Mehari <kibrommehari121@gmail.com>
2025-06-01 14:41:56 +0300

    new DTO class for sending back token and headers

--- src/main/java/org/kbapps/tigrinya_blog/dto/AuthResponse.java
+++ src/main/java/org/kbapps/tigrinya_blog/dto/AuthResponse.java
@@ -1,4 +1,12 @@
 package org.kbapps.tigrinya_blog.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AuthResponse {
+  private String token;
 }
