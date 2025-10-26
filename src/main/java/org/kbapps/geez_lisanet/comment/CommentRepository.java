04bb1d35818a9d9438678f4c5f96df0fc72c3abd
kbrom <kibrommehari121@gmail.com>
2025-10-26 15:28:23 +0300

    renamed

---
+++ src/main/java/org/kbapps/geez_lisanet/comment/CommentRepository.java
@@ -0,0 +1,10 @@
+package org.kbapps.geez_lisanet.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+   List<Comment> findCommentsByPostId(Long id);
+
+}
