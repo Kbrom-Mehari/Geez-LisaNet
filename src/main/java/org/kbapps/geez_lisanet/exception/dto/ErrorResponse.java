6a1db1995fa921a4771138744f3b95cf2f0535b0
kbrom <kibrommehari121@gmail.com>
2025-11-29 21:04:58 +0300

    moved

---
+++ src/main/java/org/kbapps/geez_lisanet/exception/dto/ErrorResponse.java
@@ -0,0 +1,34 @@
+package org.kbapps.geez_lisanet.exception.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+//@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private String error;
+    private LocalDateTime timestamp;
+    private Map<String,String> errors;
+
+    public  ErrorResponse(int status, String message, String error, LocalDateTime timestamp,Map<String,String> errors){
+        this.status=status;
+        this.message=message;
+        this.error=error;
+        this.timestamp=timestamp;
+        this.errors=errors;
+    }
+    public  ErrorResponse(int status, String message, String error, LocalDateTime timestamp){
+        this.status=status;
+        this.message=message;
+        this.error=error;
+        this.timestamp=timestamp;
+    }
+}
+
+
