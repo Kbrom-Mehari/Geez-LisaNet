76d583bb9457226683f1c580a4c253a5ec622a6e
kbrom <kibrommehari121@gmail.com>
2025-11-29 21:05:23 +0300

    moved

---
+++ src/main/java/org/kbapps/geez_lisanet/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package org.kbapps.geez_lisanet.exception.handler;
+
+import org.kbapps.geez_lisanet.exception.DuplicateEntryException;
+import org.kbapps.geez_lisanet.exception.ResourceNotFoundException;
+import org.kbapps.geez_lisanet.exception.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex){
+       return buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        Map<String,String> errors=new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error->errors.put(error.getField(),error.getDefaultMessage()));
+        return buildValidationErrorResponse(HttpStatus.BAD_REQUEST,ex.getMessage(),errors);
+    }
+    @ExceptionHandler(DuplicateEntryException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateEntryException(DuplicateEntryException ex){
+        return buildErrorResponse(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+    private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, String message){
+        ErrorResponse response=new ErrorResponse(status.value(),message,status.getReasonPhrase(), LocalDateTime.now());
+        return new ResponseEntity<>(response,status);
+    }
+    private ResponseEntity<ErrorResponse> buildValidationErrorResponse(HttpStatus status, String message, Map<String,String> errors){
+        ErrorResponse response=new ErrorResponse(status.value(),message,status.getReasonPhrase(), LocalDateTime.now(),errors);
+        return new ResponseEntity<>(response,status);
+    }
+
+
+}
